7ab0893471b55962fdde989ddbd8cd5bc12e911d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-05-23 21:27:11 +0200

    Setting tty bits

--- main.cpp
+++ main.cpp
@@ -9,7 +9,17 @@
 // Reference: https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/
 
 int main() {
+
     int serial_port = open("/dev/ttyUSB0", O_RDWR);
+
+    // Create new termios struc, we call it 'tty' for convention
+    struct termios tty;
+    memset(&tty, 0, sizeof tty);
+
+    // Read in existing settings, and handle any error
+    if(tcgetattr(serial_port, &tty) != 0) {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+    }
 
     // Check for errors
     if (serial_port < 0) {
@@ -17,4 +27,28 @@ int main() {
         // errno 13 = Permission denied
         printf("Error %i from open: %s\n", errno, strerror(errno));
     }
+
+    tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+    tty.c_cflag |= PARENB;  // Set parity bit, enabling parity
+
+    tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+    tty.c_cflag |= CSTOPB;  // Set stop field, two stop bits used in communication
+
+    tty.c_cflag |= CS5; // 5 bits per byte
+    tty.c_cflag |= CS6; // 6 bits per byte
+    tty.c_cflag |= CS7; // 7 bits per byte
+    tty.c_cflag |= CS8; // 8 bits per byte (most common)
+
+    tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+    tty.c_cflag |= CRTSCTS;  // Enable RTS/CTS hardware flow control
+
+    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+    tty.c_lflag &= ~ICANON; // Disabling canonical mode
+
+    tty.c_lflag &= ~ECHO; // Disable echo
+    tty.c_lflag &= ~ECHOE; // Disable erasure
+    tty.c_lflag &= ~ECHONL; // Disable new-line echo
+
+    tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
 }
